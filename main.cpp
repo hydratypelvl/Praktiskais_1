03b0f764f7c55b6cd06138e19f250e5f056a3d08
hydratypelvl <hydratypelvl@gmail.com>
2020-05-29 02:33:25 +0000

    Created class Auto, Made private and public access specifiers with setters and getters for each attributes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,80 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Auto {
+  private:
+    float avgFuel = 2.4;
+    string brand = "BMW";
+    string model = "X5";
+
+  public:
+    int year = 2010;
+    int hp = 335;
+
+    //FUEL
+    // Setter
+    void setFuel(float f) {
+      avgFuel = f;
+    }
+    // Getter
+    float getFuel() {
+      return avgFuel;
+    }
+    //BRAND
+    // Setter
+    void setBrand(string b) {
+      brand = b;
+    }
+    // Getter
+    string getBrand() {
+      return brand;
+    }
+    //MODEL
+    // Setter
+    void setModel(string m) {
+      model = m;
+    }
+    // Getter
+    string getModel() {
+      return model;
+    }
+    // YEAR
+    // Setter
+    void setYear(int y) {
+      year = y;
+    }
+    // Getter
+    int getYear() {
+      return year;
+    }
+    // HORSE POWER
+    // Setter
+    void setHP(int h) {
+      hp = h;
+    }
+    // Getter
+    int getHP() {
+      return hp;
+    }
+
+
+    void returnYear(){
+      cout << year;
+    }
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+  Auto myObj;
+  myObj.setFuel(6.9);
+  myObj.setBrand("Toyota");
+  myObj.setModel("Supra");
+  cout << "Fuel: " << myObj.getFuel();
+  cout << "\nBrand: " << myObj.getBrand();
+  cout << "\nModel: " << myObj.getModel();
+  cout << "\nYear: " << myObj.getYear();
+  cout << "\nYear: " << myObj.getHP();
+
+  return 0;
 }
